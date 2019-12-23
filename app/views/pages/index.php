a5a9dbe3e7d0206cd1357fc29cd74ec00b61792b
Nii Abonie Tackie <abonious@gmail.com>
2019-12-23 11:03:14 +0000

    cleaned up the framework for use

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,8 +1,3 @@
 <?php require APP_ROOT.'/views/includes/header.php';?>
     Welcome
-    <ul>
-        <?php foreach($data['posts'] as $post):?>
-            <li><?= $post->title ?>
-        <?php endforeach; ?>    
-    </ul>
 <?php require APP_ROOT.'/views/includes/footer.php';?>
