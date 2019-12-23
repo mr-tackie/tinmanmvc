51e34ddacdf647113d234e558915802640fe8907
Nii Abonie Tackie <abonious@gmail.com>
2019-12-23 11:14:52 +0000

    styled up index page a bit

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,9 @@
 <?php
     define('APP_ROOT', dirname(dirname(__FILE__)));
-    define('URL_ROOT', '_YOUR_URL_');
+    define('URL_ROOT', 'http://localhost/tinmanmvc');
     define('SITE_NAME', 'TinMan MVC');
-    
     define('DB_HOST', 'localhost');
-    define('DB_USER', '_USER_');
-    define('DB_PASSWORD', '_PASS_');
-    define('DB_NAME', '_DB_NAME_');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'tmvc');
 ?>
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,40 @@
-<?php require APP_ROOT.'/views/includes/header.php';?>
-    Welcome
-<?php require APP_ROOT.'/views/includes/footer.php';?>
+<?php require APP_ROOT . '/views/includes/header.php'; ?>
+<link href="https://fonts.googleapis.com/css?family=Inria+Serif|Source+Sans+Pro&display=swap" rel="stylesheet">
+<style>
+    *,
+    html {
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100wh;
+        height: 100vh;
+        box-sizing: border-box;
+        flex-direction: column;
+    }
+
+    h1 {
+        font-family: 'Inria Serif';
+        font-size: 52px;
+        font-weight: normal;
+    }
+
+    p {
+        font-family: 'Source Sans Pro';
+        color: #aaaaaa;
+        font-size: 16px;
+    }
+
+    a {
+        text-decoration: none;
+        color: #666666;
+        font-weight: bold;
+    }
+</style>
+<h1>Tinman MVC</h1>
+<p>An open source php framework by <a href="https://github.com/mr-tackie">Nii Abonie Tackie</a>. Lost? Read the docs on <a href="https://github.com/mr-tackie/tinmanmvc">github</a></p>
+<?php require APP_ROOT . '/views/includes/footer.php'; ?>
