d5f7a8ad530a43a134e5a06e5d8daf48bb006f07
Nii Abonie Tackie <abonious@gmail.com>
2019-12-23 11:17:26 +0000

    final cleanup of framework

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,10 @@
 <?php
     define('APP_ROOT', dirname(dirname(__FILE__)));
-    define('URL_ROOT', 'http://localhost/tinmanmvc');
+    define('URL_ROOT', '_YOUR_URL_');
     define('SITE_NAME', 'TinMan MVC');
+    
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'tmvc');
+    define('DB_USER', '_USER_');
+    define('DB_PASSWORD', '_PASS_');
+    define('DB_NAME', '_DB_NAME_');
 ?>
