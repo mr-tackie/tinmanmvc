9c790294a77f7635df51e57adf93f88b5664a1ab
Nii Abonie Tackie <abonious@gmail.com>
2019-12-23 07:40:13 +0000

    set up autoload for core classes and added app constants

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
-    require_once 'libs/Core.php';
-    require_once 'libs/Controller.php';
-    require_once 'libs/Db.php';
+    //Load config file
+    require 'config/config.php';
+
+    // Autoload Core Libraries
+    spl_autoload_register(function($className){
+        require_once 'libs/'.$className.'.php';
+    });
 ?>
---
+++ app/config/config.php
@@ -0,0 +1,5 @@
+<?php
+    define('APP_ROOT', dirname(dirname(__FILE__)));
+    define('URL_ROOT', 'http://localhost/tinymvc');
+    define('SITE_NAME', 'TinMan MVC');
+?>
